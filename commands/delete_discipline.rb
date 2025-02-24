6768741ea541d2efe5eb4d2b9b3d2bd36e89dd2c
Paprika991 <romachipurko@gmail.com>
2025-02-24 18:24:47 +0300

    Service-file has been added. Some files have been changed.

--- commands/delete_discipline.rb
+++ commands/delete_discipline.rb
@@ -1,5 +1,6 @@
 require_relative '../databases/postgresql'
 require_relative '../queries/discipline_query'
+require_relative '../services/input_discipline_name'
 
 module Commands
   class DeleteDiscipline
@@ -9,25 +10,23 @@ module Commands
 
     def call
       input_name
+      return unless discipline_exist?
+
       delete_discipline
-      puts 'Дисциплина была успешно удалена.' if @result.ntuples.positive?
     end
 
     private
 
     def input_name
-      loop do
-        puts 'Введите название Дисциплины, которую хотите удалить:'
-        @discipline_name = gets.chomp
-        break if valid_name?
-      end
+      @discipline_name = Services::InputDisciplineName.call
     end
 
-    def valid_name?
-      return puts 'Название Дисциплины не может быть пустым' if @discipline_name.nil? || @discipline_name.strip.empty?
-
-      discipline = Queries::DisciplineQuery.new.find_by_name(discipline_name: @discipline_name)
-      return puts "Дисциплина с именем #{@semester_name} не найдена." if discipline.nil?
+    def discipline_exist?
+      discipline = Queries::DisciplineQuery.new.find_by_name(
+        discipline_name: @discipline_name,
+        semester_id: @semester_id
+      )
+      return puts "Дисциплина с именем #{@discipline_name} не найдена." if discipline.nil?
 
       @discipline_id = discipline.id
       true
@@ -38,6 +37,7 @@ module Commands
         query: 'DELETE FROM disciplines WHERE id = $1 AND semester_id = $2 RETURNING name',
         params: [@discipline_id, @semester_id]
       )
+      puts 'Дисциплина была успешно удалена.' if @result.ntuples.positive?
     end
   end
 end
